c88453bd0a9ac0e165e02820bc037960a45809ee
Lamwarld <anuvarov.maxim@gmail.com>
2025-05-13 01:49:35 +0500

    u

--- src/main.py
+++
@@ -1,5 +0,0 @@
-from db.connection import *
-from ui.main_window import *
-
-
-TkinterWindow("test", (False, False), "fullscreen")
--- src/tab_frame.py
+++ src/tab_frame.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from tkinter import ttk
 
+
 class FilterTab(ttk.Frame):
     """
     Класс для реализации вкладки 'Фильтр'
